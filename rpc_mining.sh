cb138a41c328ca12a8f6e3902ce124fa6d18a0a1
khoacung <101453722+khoacung@users.noreply.github.com>
2023-03-24 21:49:03 +0700

    Delete rpc_mining.sh

--- rpc_mining.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
-while :; do
-    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmxva38u56pscaqtjtst6 -m 3 -r community-pools.mysrv.cloud:10300 -r1 dero.rabidmining.com:10300  -p rpc;
-    sleep 5;
-done
